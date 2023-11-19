141ff7763e1a8d44c2d25a03d5e1c0f03013c69f
AlDrag91 <147440379+AlDrag91@users.noreply.github.com>
2023-11-19 18:03:15 +0400

    develop 1 homework youtube

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,10 @@
+import os
+from googleapiclient.discovery import build
+from icecream import ic
+
+api_key: str = os.getenv('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+ic(youtube)
 
 
 class Channel:
@@ -5,8 +12,15 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        info_channel = youtube.channels().list(
+            part='contentDetails,snippet,statistics',
+            id='UC-OVMPlMA3-YCIeg4z5z23A'
+        )
+        result = info_channel.execute()
+        ic(info_channel)
+        ic(result)
+        # print(result)
