5e77a43e7b7a64d4ce722c5300a27114e891e2e9
AlDrag91 <147440379+AlDrag91@users.noreply.github.com>
2023-12-23 15:47:47 +0500

    homework 6 Исключения.

--- src/video.py
+++ src/video.py
@@ -9,14 +9,21 @@ class Video:
     def __init__(self, video_id):
         """Инициализация атрибутов экземпляра класса Video"""
         self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
+        """Остальную инициализация оборачиваем try/except"""
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
 
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url_video = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.url_video = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_title = None
+            self.url_video = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.video_title}'
